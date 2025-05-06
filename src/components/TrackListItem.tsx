cdc473c8edd5da4c5c1150cb68d465249950f372
aadi Katiyaan <aadikatiyaan@aadis-MacBook-Pro.local>
2025-05-07 03:07:04 +0530

    Adding the Flatlist and songs json file

---
+++ src/components/TrackListItem.tsx
@@ -0,0 +1,10 @@
+import { TouchableHighlight, View } from "react-native"
+import FastImage from "react-native-fast-image"
+
+export const TracksListItem =()=>{
+    return <TouchableHighlight>
+        <View>
+            <FastImage/>
+        </View>
+    </TouchableHighlight>
+}
