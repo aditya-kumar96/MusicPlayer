8ce64695e337cec165d20071085e7e68e20d0bf7
aadi Katiyaan <aadikatiyaan@aadis-MacBook-Pro.local>
2025-05-08 12:15:41 +0530

    Update TrackList Items UI Screen

--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -1,10 +1,64 @@
-import { TouchableHighlight, View } from "react-native"
+import { unknownArtistImageUri, unknownTrackImageUri } from "@/constants/images"
+import { colors, fontSize } from "@/constants/tokens"
+import { defaultStyles } from "@/styles"
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native"
 import FastImage from "react-native-fast-image"
 
-export const TracksListItem =()=>{
+export type TrackListItemProps = {
+    track: { title: string, image?: string ,artist?:string }
+}
+export const TracksListItem = ({ track }: TrackListItemProps) => {
+    const isActiveTrack = false
+
     return <TouchableHighlight>
         <View>
-            <FastImage/>
+            <FastImage source={{
+                uri: track.image ?? unknownTrackImageUri,
+                priority: FastImage.priority.normal 
+            }}
+                style={{
+                    ...styles.trackArtworkImage,
+                    opacity: isActiveTrack ? 0.6 : 1
+                }}
+            />
+        {/* Track Title + Artist */}
+                <View style={{width:'100%'}}>
+                    <Text 
+                    numberOfLines={1}
+                    style={{
+                        ...styles.trackTitleText,
+                        color : isActiveTrack ? colors.primary : colors.text
+                    }}
+                    >{track.title}</Text>
+                    {
+                        track.artist && (
+                            <Text
+                                numberOfLines={1}
+                                style={styles.trackArtistText}
+                            >{track.artist}</Text>
+                        )
+                    }
+                </View>
         </View>
     </TouchableHighlight>
 }
+
+const styles = StyleSheet.create({
+    trackArtworkImage: {
+        borderRadius: 8,
+        width: 50,
+        height: 50
+    },
+    trackTitleText:{
+        ...defaultStyles.text,
+        fontSize:fontSize.sm,
+        fontWeight:'600',
+        maxWidth:'90%'
+    },
+    trackArtistText:{
+        ...defaultStyles.text,
+        color:colors.textMuted,
+        fontSize:14,
+        marginTop:4
+    }
+})
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,9 +1,8 @@
 import { FlatList } from "react-native"
 import library from '@/assets/data/library.json'
-import { TracksListItem } from "./TrackListItem"
 export const TracksList=()=>{
     return <FlatList
         data={library}
-        renderItem={()=> <TracksListItem />}
+        renderItem={()=>null}
     />
 }
